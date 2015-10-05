be7f07cbef5395888c472328edf7d113e7668593
Steve Minor <steve.e.minor@nordstrom.com>
2015-10-05 11:06:22 -0700

    hooked up OSC socket communication between server and host application

--- bin/server/server.js
+++ bin/server/server.js
@@ -1,13 +1,13 @@
-var converter = require('./converter.js'),
-	express = require('express'),
+var express = require('express'),
 	app = express(),
 	http = require('http').Server(app),
 	dgram = require('dgram'),
 	osc = require('osc-min'),
-	// sendSocket = dgram.createSocket('udp4'),
-	// receiveSocket = dgram.createSocket('udp4'),
+	sendSocket = dgram.createSocket('udp4'),
+	receiveSocket = dgram.createSocket('udp4'),
 	SEND_PORT = 12345,
-	RECEIVE_PORT = 12346;
+	RECEIVE_PORT = 12346,
+	OUTPUT_DIR = "../data/";
 
 sendSocket.bind(SEND_PORT);
 receiveSocket.bind(RECEIVE_PORT);
@@ -15,37 +15,53 @@ receiveSocket.bind(RECEIVE_PORT);
 function getOSCMessage(msg){
 	//extract relevant data from OSC Message
 	var oscMessage = osc.fromBuffer(msg);
-
 	try{
 		console.log(oscMessage);
 		// //translate osc buffer into javascript object
-		// var element = oscMessage.elements[0],
-		// address = element.address,
-		// args = element.args; //contains 'type' and 'value
+		var element = oscMessage.elements[0],
+			address = element.address,
+			args = element.args,
+			cleanMovie = args[0].value,
+			distortedMovie = args[1].value;
 
-		// return {
-		// address: address,
-		// filename: args[0].value,
-		// id : args[1].value
-		// }
+		return {
+			address: address,
+			cleanMovie: cleanMovie,
+			distortedMovie: distortedMovie
+		};
 
 	}catch(error){
 		console.log("invalid OSC packet");
 	}
 }
 
-function sendOSCMessage(mov_filename){
+receiveSocket.on('message', function(message, remote){
+	var oscData = getOSCMessage(message);
+	// looks like this 
+	// {
+	// 	address: '/video',
+	// 	cleanMovie: 'filename.mp4',
+	// 	distortedMovie: 'filename_distorted.mp4'
+	// }
+	// console.log(oscData);
+
+	//find the files using OUTPUT_DIR and filenames
+	//send a post request to S3 with resources
+	//on complete, call sendOSCMessage with the code
+});
+
+function sendOSCMessage(code){
 	var buffer = osc.toBuffer({
-		address: '/take/picture',
+		address: '/uploaded',
 		args : [{
 			type: "string",
-			value: mov_filename
+			value: code
 		}]
 	});
-	sendSocket.send(buffer, 0, buffer.length, SEND_PORT, 'localhost');
+	sendSocket.send(buffer, 0, buffer.length, SEND_PORT, '127.0.0.1');
 }
 
 var sendInterval = setInterval(function(){
-	sendOSCMessage('converted_movie.mov');
+	sendOSCMessage('1337H4X04L0L0L0L0L');
 }, 3000);
 
